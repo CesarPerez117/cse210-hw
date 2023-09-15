bea89bb62873f50a1c46a9a3e2756e55a3c68196
CesarPerez117 <cesarperezp117@gmail.com>
2023-09-15 18:45:34 -0400

    prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int userPercentage = int.Parse(userAnswer);
+        string letter = "";
+
+        if (userPercentage >= 90 && userPercentage < 100)
+        {
+            letter = "A";
+
+            if (userPercentage >= 97)
+            {
+                letter = "A+";
+            }
+            else if (userPercentage <= 93)
+            {
+                letter = "A-";
+            }
+        }
+        else if (userPercentage >= 80 && userPercentage < 90)
+        {
+            letter = "B";
+
+            if (userPercentage >= 87)
+            {
+                letter = "B+";
+            }
+            else if (userPercentage <= 83)
+            {
+                letter = "B-";
+            }
+        }
+        else if (userPercentage >= 70 && userPercentage < 80)
+        {
+            letter = "C";
+
+            if (userPercentage >= 77)
+            {
+                letter = "C+";
+            }
+            else if (userPercentage <= 73)
+            {
+                letter = "C-";
+            }
+        }
+        else if (userPercentage >= 60 && userPercentage < 70)
+        {
+            letter = "D";
+
+            if (userPercentage >= 67)
+            {
+                letter = "D+";
+            }
+            else if (userPercentage <= 63)
+            {
+                letter = "D-";
+            }
+        }
+        else if (userPercentage < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Syntax Error");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (userPercentage > 70)
+        {
+            Console.WriteLine("Congratulations, you have pass the course!");
+        }
+        else if (userPercentage < 70)
+        {
+            Console.WriteLine("Sorry, you fail the course. Encourage next time.");
+        }
     }
 }
