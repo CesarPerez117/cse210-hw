6a5d00aecd49610c21740186e24d4a2ad0c7320c
CesarPerez117 <cesarperezp117@gmail.com>
2023-09-15 17:11:48 -0400

    prep1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        /*Assignment
+        Prompt the user for their first name. Then, prompt them for their last name. Display the text back all on one line saying, 
+        "Your name is last-name, first-name, last-name" as shown:
+
+        What is your first name? Scott
+        What is your last name? Burton
+
+        Your name is Burton, Scott Burton.
+
+        What is your first name? Brigham
+        What is your last name? Young
+
+        Your name is Young, Brigham Young.
+
+        Make sure to be precise! You should have the spacing, comma, and period appear exactly as shown in the examples.
+        */
     }
 }
