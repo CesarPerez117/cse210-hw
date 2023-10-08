08109eeb05f41c4d2dfe16c8bf5878cf5e3230cb
CesarPerez117 <cesarperezp117@gmail.com>
2023-10-07 22:24:25 -0400

    comments

--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -1,15 +1,15 @@
 using System;
 
-public class JournalEntry
+public class JournalEntry // JournalEntry class can be use it every time an user is writting a new journal entry.
 {
-    public string _dateTime = "";
-    public string _journalPrompt = "";
-    public string _journalAnswer = "";
+    public string _dateTime = ""; // takes the time and date where the user entered the info.
+    public string _journalPrompt = ""; // takes the prompt or question the user will answer. 
+    public string _journalAnswer = ""; // takes what the user answered
     public JournalEntry()
     {
     }
 
-    public void Display()
+    public void Display() // organize the data and display it
     {
         Console.WriteLine($"Date: {_dateTime} - {_journalPrompt}");
         Console.WriteLine($"> {_journalAnswer}");
